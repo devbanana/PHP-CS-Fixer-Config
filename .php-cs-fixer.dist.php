5d1987ac69b2b59d8922a105442a3ee235e9fa35
Brandon Olivares <programmer2188@gmail.com>
2021-08-22 00:34:15 -0400

    Add license header

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the devbanana/php-cs-fixer-config package.
+ *
+ * (c) Brandon Olivares <programmer2188@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Devbanana\FixerConfig\Configurator;
 use Devbanana\FixerConfig\PhpVersion;
 use PhpCsFixer\Finder;
@@ -13,8 +22,22 @@ $finder = Finder::create()
     ])
 ;
 
+$header = <<<'EOF'
+    This file is part of the devbanana/php-cs-fixer-config package.
+
+    (c) Brandon Olivares <programmer2188@gmail.com>
+
+    For the full copyright and license information, please view the LICENSE
+    file that was distributed with this source code.
+    EOF;
+
 return Configurator::fromPhpVersion(PhpVersion::PHP_80())
     ->withRiskyRulesEnabled()
-    ->configuration()
+    ->withAddedRules([
+        'header_comment' => [
+            'header' => $header,
+        ],
+    ])
+    ->fixerConfig()
     ->setFinder($finder)
 ;
